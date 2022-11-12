983a95de8dd9a04fe7f3e437acad2af36f96abb0
Dmitry Agibalov <theverilly@gmail.com>
2022-11-12 13:11:16 +0200

    connect fonts from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import './assets/css/reset.css';
 import './assets/css/index.css';
+
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <StrictMode>
